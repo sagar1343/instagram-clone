db2f9603834eb7275699c80b2439ae0c630f6491
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2026-07-10 12:55:18 +0530

    created post card

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request=request, template_name="index.html", context={"posts": posts})
